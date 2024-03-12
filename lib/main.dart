6693655fcfa8f2ee8cce9a1ab368f8c0cab4dfca
Sunil <sunilkumarbsrrs@gmail.com>
2024-03-12 14:25:06 +0530

    This is my Third Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ToDo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
@@ -49,17 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-
-          ],
-        ),
       ),
     );
   }
