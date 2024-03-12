a48398cf0349c5992c8260b249a8d3c417a32eda
Sunil <sunilkumarbsrrs@gmail.com>
2024-03-12 14:20:14 +0530

    This is my Second Commit.

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -13,12 +13,7 @@ class ToDo{
 
   static List<ToDo> todoList(){
     return[
-      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Grocerries', isDone: true),
-      ToDo(id: '03', todoText: 'Check Mail', ),
-      ToDo(id: '04', todoText: 'Team Meating' ),
-      ToDo(id: '05', todoText: 'Work on Mobile apps for 3 hrors',),
-      ToDo(id: '06', todoText: 'Dinner with Anand',),
+
     ];
   }
 }
